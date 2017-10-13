b9db4defbb1cdf245c066da04e819dd69ef62ba3
Adam Bergman <adam@fransvilhelm.com>
2017-10-13 13:53:58 +0200

    :bug: Remove leftover code

--- pages/nyhetsbrevet/index.js
+++ pages/nyhetsbrevet/index.js
@@ -3,13 +3,8 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 
-const Nyhetsbrevet = () => (
+export default () => (
   <Layout activeLink={'/nyhetsbrevet'} user={{ name: 'Adam Bergman' }}>
     <h1>Nyhetsbrevet</h1>
   </Layout>
 );
-
-Nyhetsbrevet.getInitialProps = () =>
-  new Promise(resolve => setTimeout(() => resolve({}), 5000));
-
-export default Nyhetsbrevet;
