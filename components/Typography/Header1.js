20ca185941feb7df17866b16c743f163dd6b47bc
Adam Bergman <adam@fransvilhelm.com>
2017-10-17 10:41:16 +0200

    :art: Add Header components

---
+++ components/Typography/Header1.js
@@ -0,0 +1,11 @@
+// @flow
+import styled from 'styled-components';
+
+export default styled.h1`
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+  font-weight: 400;
+  font-size: 2.25em;
+  letter-spacing: 0.05em;
+  color: #999;
+`;
