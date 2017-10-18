9db2385f5d619778f0713407050a289eab6e96a6
Adam Bergman <adam@fransvilhelm.com>
2017-10-18 16:28:34 +0200

    :construction: WIP

--- components/Tidningen/YearView/YearIssue.js
+++ components/Tidningen/YearView/YearIssue.js
@@ -4,17 +4,42 @@ import styled from 'styled-components';
 import raf from 'raf-schd';
 import Loader from '../../Loader';
 import LazyImage from '../../LazyImage';
+// import Link from '../../Link';
 import type { Issue } from '../../../store/tidningen/types';
 
-const IssueContainer = styled.div`
+const IssueContainer = styled.button`
   display: inline-block;
   width: calc((100% - ${props => props.theme.size(0) * 3}em) / 4);
   margin-right: ${props => props.theme.size(0)}em;
   margin-bottom: ${props => props.theme.size(0)}em;
+  border: none;
+  border-radius: 0;
+  padding: 0;
+  font-size: 1em;
+  cursor: pointer;
   z-index: 1;
 
   &:nth-child(4n) {
     margin-right: 0;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover p,
+  &:focus p {
+    color: transparent;
+  }
+
+  &:hover p::after,
+  &:focus p::after {
+    transform: translateY(0);
+  }
+
+  &:hover > div,
+  &:focus > div {
+    border-color: ${props => props.theme.color.brand};
   }
 `;
 
@@ -23,7 +48,9 @@ const ImgContainer = styled.div`
   width: 100%;
   height: 0;
   border: 1px solid ${props => props.theme.color.grey};
+  border-radius: 0;
   padding-top: calc(100% * ${props => props.theme.pageAspectRatio});
+  transition: border 0.3s ease-in-out;
 `;
 
 const Img = styled(LazyImage)`
@@ -48,13 +75,31 @@ const ImgLoader = styled(Loader)`
   transition: opacity 0.3s ease-in-out;
 `;
 
-const Desc = styled.p`
+const IssueLink = styled.p`
+  position: relative;
   margin: 0;
   margin-top: ${props => props.theme.size(-1)}em;
   font-family: ${props => props.theme.font.serif};
   font-size: ${props => props.theme.size(-1)}em;
   text-align: center;
   color: ${props => props.theme.color.grey};
+  overflow: hidden;
+  will-change: color;
+  transition: color 0.3s ease-in-out;
+
+  &::after {
+    content: 'Visa';
+    position: absolute;
+    display: block;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    color: ${props => props.theme.color.brand};
+    transform: translateY(100%);
+    will-change: transform;
+    transition: transform 0.3s ease-in-out;
+  }
 `;
 
 type Props = { issue?: Issue };
@@ -82,7 +127,9 @@ export default class YearIssue extends Component<Props, State> {
           )}
           <ImgLoader show={loading} />
         </ImgContainer>
-        <Desc>{issue && !loading ? `Nummer ${issue.name}` : 'Laddar'}</Desc>
+        <IssueLink>
+          {issue && !loading ? `Nummer ${issue.name}` : 'Laddar'}
+        </IssueLink>
       </IssueContainer>
     );
   }
