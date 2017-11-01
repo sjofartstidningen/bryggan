711063e04c7cae58fc866cc0ef174287a9558a12
Adam Bergman <adam@fransvilhelm.com>
2017-11-01 14:37:45 +0100

    :art: Add Auth0 variables

--- env-config.sample.js
+++ env-config.sample.js
@@ -1,4 +1,6 @@
 module.exports = {
   'process.env.NODE_ENV': process.env.NODE_ENV || 'development',
   'process.env.DROPBOX_ACCESS_TOKEN': 'DROPBOX_ACCESS_TOKEN',
+  'process.env.AUTH0_CLIENT_ID': 'AUTH0_CLIENT_ID',
+  'process.env.AUTH0_CLIENT_DOMAIN': 'AUTH0_CLIENT_DOMAIN',
 };
