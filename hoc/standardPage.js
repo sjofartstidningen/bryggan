d2bf58101909e5a40835c576c686bdf2ae73aeeb
Adam Bergman <adam@fransvilhelm.com>
2017-11-02 16:12:18 +0100

    :art: Update standardPage

--- hoc/standardPage.js
+++ hoc/standardPage.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import withRedux from 'next-redux-wrapper';
 import { initStore } from '../store';
 import Layout from '../components/Layout';
 
 export default Page => {
   class Standard extends Component {
+    static propTypes = {
+      title: PropTypes.string,
+    };
+
+    static defaultProps = {
+      title: 'Bryggan – Sjöfartstidningen',
+    };
+
     static async getInitialProps(ctx) {
       const pageProps = Page.getInitialProps
         ? await Page.getInitialProps({ ...ctx })
