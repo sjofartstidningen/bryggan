e68a3c56371a663861d26f3424fbe7c0ec8b377d
Adam Bergman <adam@fransvilhelm.com>
2017-11-02 17:12:44 +0100

    :bug: Fix sign-out page not working

--- pages/auth/sign-out.js
+++ pages/auth/sign-out.js
@@ -3,22 +3,14 @@ import Router from 'next/router';
 import AuthService from '../../utils/auth';
 
 export default class SignOut extends Component {
-  static async getInitialProps(ctx) {
-    const auth = new AuthService(
+  componentDidMount() {
+    this.auth = new AuthService(
       process.env.AUTH0_CLIENT_ID,
       process.env.AUTH0_CLIENT_DOMAIN,
     );
 
-    auth.logout();
-
-    if (ctx.isServer) {
-      ctx.res.writeHead(302, { Location: '/auth/sign-in' });
-      ctx.res.end();
-      return {};
-    }
-
+    this.auth.logout();
     Router.push('/auth/sign-in');
-    return {};
   }
 
   // eslint-disable-next-line
