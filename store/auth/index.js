25329dbc722d3364ff4cc1839146b4260eccaa3d
Adam Bergman <adam@fransvilhelm.com>
2017-11-02 17:54:32 +0100

    :sparkles: Update auth store

--- store/auth/index.js
+++ store/auth/index.js
@@ -1,13 +1,46 @@
+import * as constants from './constants';
+
 const initialState = {
   signedIn: false,
+  user: null,
+  tokens: {},
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'SIGNED_IN':
+    case constants.SIGN_IN:
+      console.log(constants.SIGN_IN);
+      console.log(action);
       return {
         ...state,
         signedIn: true,
+      };
+
+    case constants.SIGN_OUT:
+      console.log(constants.SIGN_OUT);
+      console.log(action);
+      return initialState;
+
+    case constants.ADD_TOKEN:
+      console.log(constants.ADD_TOKEN);
+      console.log(action);
+      return {
+        ...state,
+        tokens: {
+          ...state.tokens,
+          [action.payload.token]: action.payload.value,
+        },
+      };
+
+    case constants.UPDATE_USER:
+      console.log(constants.UPDATE_USER);
+      console.log(action);
+      return {
+        ...state,
+        user: {
+          ...(state.user || {}),
+          ...action.payload.user,
+        },
       };
 
     default:
