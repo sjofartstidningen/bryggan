67eb01db022907ab82da4f348e557aa9254bba7d
Adam Bergman <adam@fransvilhelm.com>
2017-11-24 06:55:03 +0100

    :fire: Remove old implementation

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,9 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import tidningen from './tidningen';
-import auth from './auth';
 import user from './user';
 
-const reducer = combineReducers({ tidningen, auth, user });
+const reducer = combineReducers({ tidningen, user });
 
 const initStore = (state = {}) =>
   createStore(reducer, state, composeWithDevTools(applyMiddleware(thunk)));
