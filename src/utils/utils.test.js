be0bcdf90178de7102ecc7699b000b5d562862e9
Adam Bergman <adam@fransvilhelm.com>
2018-02-02 16:06:05 +0100

    test: Add tests

--- src/utils/utils.test.js
+++
@@ -1,8 +0,0 @@
-import { getThumbnailSize } from './dropbox';
-
-test('dropbox.getThumbnailSize', () => {
-  expect(getThumbnailSize(960)).toBe('w960h640');
-  expect(getThumbnailSize(960, 2)).toBe('w2048h1536');
-  expect(getThumbnailSize(368)).toBe('w480h320');
-  expect(getThumbnailSize(3000)).toBe('w2048h1536');
-});
