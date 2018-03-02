71f5ea77a049bcbe6692fb1a4c94747fa74873d2
Adam Bergman <adam@fransvilhelm.com>
2018-03-02 15:46:35 +0100

    chore(storybook): Setup Storybook

---
+++ .storybook/config.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import * as theme from '../src/theme';
+
+addDecorator(story => (
+  <Router>
+    <ThemeProvider theme={theme}>{story()}</ThemeProvider>
+  </Router>
+));
+
+const req = require.context('../src', true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
