f028a785954ddf168b2e547cc5b4c0971665fc8c
Adam Bergman <adam@fransvilhelm.com>
2018-03-07 16:33:08 +0100

    feat(atoms): Add Image atom

---
+++ src/atoms/Image/index.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import {
+  applyStyleModifiers,
+  styleModifierPropTypes,
+} from 'styled-components-modifiers';
+
+const modifierConfig = {
+  rounded: () => `border-radius: 100%;`,
+};
+
+const Img = styled.img`
+  display: block;
+  max-width: 100%;
+  height: auto
+  border-style: none;
+
+  ${applyStyleModifiers(modifierConfig)};
+`;
+
+Img.propTypes = {
+  modifiers: styleModifierPropTypes(modifierConfig),
+};
+
+export { Img };
