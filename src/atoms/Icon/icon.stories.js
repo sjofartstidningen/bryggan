20a696a5946ab291f678b5feff76633b30110275
Adam Bergman <adam@fransvilhelm.com>
2018-03-12 15:33:40 +0100

    fix: Use Object.entries

--- src/atoms/Icon/icon.stories.js
+++ src/atoms/Icon/icon.stories.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-// @flow
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import * as Icon from './index';
@@ -7,25 +6,29 @@ import * as Icon from './index';
 storiesOf('atoms/Icon', module)
   .add('all', () => (
     <div>
-      {Object.keys(Icon).map(key => {
-        const Comp = Icon[key];
-        return (
+      {Object.entries(Icon).map(
+        ([key, Comp]) => (
           <div key={key} style={{ margin: '1rem', fontFamily: 'Roboto' }}>
-            <Comp /> – {'<'}{key}{' />'}
+            <Comp /> – {'<'}
+            {key}
+            {' />'}
           </div>
-        );
-      }, [])}
+        ),
+        [],
+      )}
     </div>
   ))
   .add('without basline alignment', () => (
     <div>
-      {Object.keys(Icon).map(key => {
-        const Comp = Icon[key];
-        return (
+      {Object.entries(Icon).map(
+        ([key, Comp]) => (
           <div key={key} style={{ margin: '1rem', fontFamily: 'Roboto' }}>
-            <Comp baseline={false} /> – {'<'}{key}{' baseline={false} />'}
+            <Comp baseline={false} /> – {'<'}
+            {key}
+            {' baseline={false} />'}
           </div>
-        );
-      }, [])}
+        ),
+        [],
+      )}
     </div>
   ));
