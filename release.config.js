ba91c45576f30309f1a8b1429b0bb25fdbddb355
Adam Bergman <adam@fransvilhelm.com>
2018-03-22 12:26:19 +0100

    chore: Update release settings

--- release.config.js
+++ release.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   verifyConditions: ['@semantic-release/github'],
-  prepare: [require.resolve('./scripts/release-prepare.js')],
-  publish: ['@semantic-release/github'],
+  publish: [
+    '@semantic-release/github',
+    {
+      path: '@semantic-release/exec',
+      cmd: `curl -X POST -d '' ${process.env.NETLIFY_WEBHOOK}`,
+    },
+  ],
   success: ['@semantic-release/github'],
   fail: ['@semantic-release/github'],
 };
