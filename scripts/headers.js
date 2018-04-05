81895e6dc6b422f13b7f5e1f71cb294aff8aa849
Adam Bergman <adam@fransvilhelm.com>
2018-04-05 09:45:51 +0200

    chore: Add serve-with-headers script
    Used to test new headers.

---
+++ scripts/headers.js
@@ -0,0 +1,20 @@
+module.exports = {
+  'Content-Security-Policy-Report-Only': [
+    `default-src 'self'`,
+    `script-src 'self' https://*.firebaseio.com/ https://unpkg.com`,
+    `style-src 'self' 'unsafe-inline' blob: https://fonts.googleapis.com/`,
+    `font-src 'self' https://fonts.gstatic.com/`,
+    `img-src 'self' data blob: https://content.dropboxapi.com/ https://www.gravatar.com/avatar/`,
+    `connect-src 'self' blob: wss://*.firebaseio.com/ https://*.googleapis.com/ https://*.dropboxapi.com/`,
+    `frame-src 'self' https://*.firebaseio.com/`,
+    `object-src 'none'`,
+    `worker-src 'self' blob:`,
+    `report-uri https://sjofartstidningen.report-uri.com/r/d/csp/reportOnly`,
+  ].join('; '),
+  'X-Frame-Options': 'SAMEORIGIN',
+  'X-XSS-Protection': '1; mode=block',
+  'X-Content-Type-Options': 'nosniff',
+  'Referrer-Policy': 'no-referrer-when-downgrade',
+  'Expect-CT':
+    'max-age=0, report-uri="https://sjofartstidningen.report-uri.com/r/d/ct/reportOnly"',
+}
