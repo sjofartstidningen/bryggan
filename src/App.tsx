8b30e62992d63ce806df77ab2796ab641d2e4824
Adam Bergman <adam@fransvilhelm.com>
2019-05-15 21:51:27 +0200

    chore: Setups

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const App: React.FC = () => {
-  return <div />;
+  return (
+    <div>
+      <div>
+        <header>
+          <div>
+            <h1>Bryggan</h1>
+          </div>
+        </header>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="/tidningen">Tidningen</a>
+            </li>
+            <li>
+              <a href="/installningar">Inställningar</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div>
+          <div>
+            <div>Adam Bergman</div>
+            <div>
+              <a href="/sign-out">Logga ut</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/setupTests.ts
@@ -0,0 +1,5 @@
+import 'react-testing-library/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+// @link https://facebook.github.io/create-react-app/docs/running-tests#src-setuptestsjs
+export default undefined;
